a07d6d5354d68401a8ca1eb3fa3935bc1217f19e
Adão José <adao.jose123.a.r@gmail.com>
2020-06-16 00:00:16 -0400

    .

--- AR/activeRecord/ActiveRecord.php
+++ AR/activeRecord/ActiveRecord.php
@@ -95,7 +95,7 @@ abstract class ActiveRecord {
      * }) 
      */
     public function eatch($calback){
-        foreach ($this->toArray() as $value) {
+        foreach ($this->content as $value) {
             
             call_user_func($calback, $value);
             
